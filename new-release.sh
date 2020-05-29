e230e3df375e69b52c2ad894a2fc3a880df2218b
James Kyburz <james.kyburz@gmail.com>
2020-05-29 15:30:34 +0200

    feature: add new-release.sh

--- new-release.sh
+++ new-release.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
+
 set -ueo pipefail
 
-log_success() { echo -e "\033[0m\033[1;92m${*:1}\033[0m"; }
+log_success() { echo -e "\033[0m\033[1;92m${*}\033[0m"; }
 
-current_tag=$(git describe --tags $(git rev-list --tags --max-count=1))
+current_tag=$(git describe --tags "$(git rev-list --tags --max-count=1)")
 
-read -p "Tag [patch]? " new_tag
+read -p "Tag [patch]? " -r new_tag
 
 here=$(pwd)
 
 tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 
-cd ${tmp_dir:?}
+cd "${tmp_dir:?}"
 
 npm init -y 1>/dev/null 2>&1
 git init
-npm version ${current_tag:?}
-npm version ${new_tag:-patch}
+npm version "${current_tag:?}"
+npm version "${new_tag:-patch}"
 new_tag=$(git describe --tag)
 
-cd ${here:?}
+cd "${here:?}"
 
-git tag ${new_tag:?}
+git tag "${new_tag:?}"
 
 log_success "created git tag ${new_tag:?}"
 
